d38912ac67ba0d2e52a693f22484b0b411ec49c7
Eileen-C <eileenchenvv@gmail.com>
2022-08-17 14:57:16 +0800

    Lesson-09 test004 ok

--- Lesson-09/text_ctrl.js
+++ Lesson-09/text_ctrl.js
@@ -7,7 +7,7 @@ $(function(){
 	});
 	wow.init();
 	$("#js-rotating").Morphext({
-		animation: "fadeIn", // Overrides default "bounceIn"
+		animation: "wobble", // Overrides default "bounceIn"
 		separator: ",", // Overrides default ","
 		speed: 3000, // Overrides default 2000
 		complete: function () {
