45ad8c1cd34c362a61a5dda13c499b635be963e4
Eileen-C <eileenchenvv@gmail.com>
2022-08-17 18:28:43 +0800

    jumbotron、card、pagination

---
+++ Lesson-09/index01.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>林肯運動鞋專賣店-電子商店</title>
+    <!-- bootstrap 4.6.2 css -->
+    <link rel="stylesheet" href="./bootstrap-4.6.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./animate.css">
+    <link rel="stylesheet" href="./morphext.css">
+    <link rel="stylesheet" href="./website01.css">
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="video-background">
+            <iframe src="https://www.youtube.com/embed/F_dGEjzRG_Y?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&playlist=F_dGEjzRG_Y" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="video-caption text-center">
+            <div class="wow rotateIn" data-wow-offset="1" data-wow-delay="1s">
+                <h1><span id="js-rotating">全台最專業運動鞋購物平台,不出門也能盡享購物樂趣,多款跑鞋低價超值折扣,型男美女必買商品！多款知名品牌運動鞋隨你挑,全館激殺價,千萬別錯過,加入會員首次購物拿500,12H速達早上訂下午到貨</span></h1>
+            </div>
+            <div class="wow bounceInUp" data-wow-offset="0" data-wow-delay="1s">
+                <div class="margintop-30"><a href="main.php" class="btn btn-warning">Start here</a></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jquery and bootstrap javascript -->
+    <script type="text/javascript" src="./bootstrap-4.6.2-dist/js/jquery.min.js"></script>
+    <script type="text/javascript" src="./bootstrap-4.6.2-dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="./wow.js"></script>
+    <script type="text/javascript" src="./morphext.js"></script>
+    <script type="text/javascript" src="./text_ctrl.js"></script>
+</body>
+
+</html>
---
+++ Lesson-09/main.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>林肯運動鞋專賣店-電子商店</title>
+    <!-- bootstrap 4.6.2 css -->
+    <link rel="stylesheet" href="./bootstrap-4.6.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./animate.css">
+    <link rel="stylesheet" href="./morphext.css">
+    <link rel="stylesheet" href="./website01.css">
+
+</head>
+
+<body>
+    <section id="mainmenu">
+        <div class="container-fluid">
+            <div class="row text-center">
+                <div class="jumbotron">
+                    <h1 class="display-4">！讓我們一起動起來！</h1>
+                    <p class="lead">在運動出汗的過程中，皮膚的新陳代謝加快，毒素排出體外，而且出汗還能夠幫助清潔毛孔，有效護膚美容。</p>
+                    <hr class="my-4">
+                    <p>讓我們提供一雙好的運動鞋來幫助你。</p>
+                    <p>林肯運動鞋專賣店-電子商店</p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Shop more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section id="production">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-xl-12">
+                    <h2>站長推薦熱銷運動鞋</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card col-md-4">
+                    <img src="./images/pic61.jpg" alt="Adidas 休閒慢跑鞋" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">adidas 休閒慢跑鞋</h5>
+                        <p class="card-text">Loop Racer 全黑 復古 運動鞋 男鞋 女鞋 B42446，愛迪達 Originals 三葉草 輕量透氣舒適運動球鞋穿搭推薦,男女款情侶鞋</p>
+                        <p>NT$ 2,500元</p>
+                        <a href="#" class="btn btn-primary">更多資訊</a>
+                        <a href="#" class="btn btn-skyblue">加購物車</a>
+                    </div>
+                </div>
+                <div class="card col-md-4">
+                    <img src="./images/pic62.jpg" alt="Reebok 休閒慢跑鞋" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Reebok 休閒慢跑鞋</h5>
+                        <p class="card-text">Reebok Insta Pump Fury SP 藍 灰 麂皮 潑漆 雪花 休閒鞋 男鞋 平底鞋 休閒鞋 慢跑鞋 球鞋 情侶運動鞋</p>
+                        <p>NT$ 4,580元</p>
+                        <a href="#" class="btn btn-primary">更多資訊</a>
+                        <a href="#" class="btn btn-skyblue">加購物車</a>
+                    </div>
+                </div>
+                <div class="card col-md-4">
+                    <img src="./images/pic63.jpg" alt="Nike 阿甘鞋" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Nike 阿甘鞋</h5>
+                        <p class="card-text">Wmns Classic Cortez Leather 復古慢跑鞋 白藍紅 OG 原版配色 女鞋 漢娜妞 松本惠奈 女孩球鞋穿搭推薦款式 百搭熱銷款 皮面不容易髒</p>
+                        <p>NT$ 3,380元</p>
+                        <a href="#" class="btn btn-primary">更多資訊</a>
+                        <a href="#" class="btn btn-skyblue">加購物車</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card col-md-4">
+                    <img src="./images/pic64.jpg" alt="Puma 慢跑鞋" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Puma 慢跑鞋</h5>
+                        <p class="card-text">Speed 600 Ignite Pwrcool Wn 黃銀 運動 女鞋 188521-02 Running 超輕量透氣 低筒 跑步健身訓練推薦鞋款</p>
+                        <p>NT$ 2,880元</p>
+                        <a href="#" class="btn btn-primary">更多資訊</a>
+                        <a href="#" class="btn btn-skyblue">加購物車</a>
+                    </div>
+                </div>
+                <div class="card col-md-4">
+                    <img src="./images/pic65.jpg" alt="Asics 慢跑鞋鞋" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Asics 慢跑鞋</h5>
+                        <p class="card-text">競速跑鞋 Tartherzeal 4 藍 橘 白底 男鞋 輕量 運動鞋 TJR282-5230 Racing 低筒 虎走 馬拉松 路跑推薦款式 特殊專業跑鞋設計</p>
+                        <p>NT$ 3,490元</p>
+                        <a href="#" class="btn btn-primary">更多資訊</a>
+                        <a href="#" class="btn btn-skyblue">加購物車</a>
+                    </div>
+                </div>
+                <div class="card col-md-4">
+                    <img src="./images/pic66.jpg" alt="美津濃 Mizuno Prophecy 5" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">美津濃 Mizuno Prophecy 5</h5>
+                        <p class="card-text">美津濃 Mizuno Prophecy 5藍黃 彈簧 慢跑鞋 男鞋 J1GC16-0047 Running U4ic中底材質,X10耐磨INFINITY WAVE大底</p>
+                        <p>NT$ 5,080元</p>
+                        <a href="#" class="btn btn-primary">更多資訊</a>
+                        <a href="#" class="btn btn-skyblue">加購物車</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row pt-2">
+                <nav aria-label="..." class="mx-auto">
+                    <ul class="pagination">
+                        <li class="page-item disabled">
+                            <a class="page-link">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item active" aria-current="page">
+                            <a class="page-link" href="#">2</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </section>
+    <!-- jquery and bootstrap javascript -->
+    <script type="text/javascript" src="./bootstrap-4.6.2-dist/js/jquery.min.js"></script>
+    <script type="text/javascript" src="./bootstrap-4.6.2-dist/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="./wow.js"></script>
+    <script type="text/javascript" src="./morphext.js"></script>
+    <script type="text/javascript" src="./text_ctrl.js"></script>
+</body>
+
+</html>
